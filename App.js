525693d404304c2ff9a963cc5e529d976d2fd073
Koray Erdem <developerkoray@gmail.com>
2021-05-20 23:09:45 +0300

    I wrote 'hello world' on android

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text style={{color:'red'}}>Hello World</Text>
+    </View>
+  );
+} 
+
+export default App;
+
