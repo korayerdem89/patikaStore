aff0fbec1ae32a5376e379635c79a313235c769f
Koray Erdem <developerkoray@gmail.com>
2021-05-21 22:52:24 +0300

    flex exercise

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 
-import {View, Text, SafeAreaView, Button, StyleSheet} from 'react-native';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
-  function sayHello() {
-    console.log('merhabalar');
-  }
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Text>Hello World 31</Text>
-      </View>
-      
-      <Button title="Here Press Me!" onPress={sayHello} />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box_1} />
+
+      <View style={styles.box_2} />
+
+      <View style={styles.box_3} />
+
+      <View style={styles.box_4} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 0.5,
+    backgroundColor: 'cyan',
+    justifyContent:'center',
+  },
+  box_1: {
+    backgroundColor: 'blue',
+    width: 75,
+    height: 75,
+  },
+  box_2: {
+    backgroundColor: 'red',
+    width: 75,
+    height: 75,
+  },
+  box_3: {
     backgroundColor: 'purple',
+    width: 75,
+    height: 75,
+  },
+  box_4: {
+    backgroundColor: 'yellow',
+    width: 75,
+    height: 75,
   },
 });
 export default App;
